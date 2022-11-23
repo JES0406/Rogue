39bd34e87cbc230a31e8fbab9f47adde5b137d66
Marcos <202204116@alu.comillas.edu>
2022-11-23 16:05:56 +0100

    Update movimiento.py

--- Código/movimiento.py
+++ Código/movimiento.py
@@ -1,8 +1,8 @@
-import pygame, math
+import pygame, math, random
 pygame.init
 
 
-
+print('Espacio para hacer que aparezca un enemigo al azar')
 ### Player variables
 pwidth = 15
 pheight = 15
@@ -21,14 +21,40 @@ def setspeed(): ### Si el jugador se desplaza a la vez en los ejes x e y, tiene 
     else: movespeed = pspeed
     return int(movespeed)
 
-## Enemy variables
-melee_epos = [200, 200]
-melee_espeed = 2.5
-melee_range = 20
 
-ranged_epos = [400, 100]
-ranged_espeed = 1.5
-kiting_distance = 200 ### Distancia a la que se quedan los enemigos con rango del jugador
+
+enemy_list = []
+enemy_id = 0
+class enemy:
+    def __init__(self, speed, range, position:tuple, type):
+        self.type = type
+        self.speed = speed
+        self.range = range
+        self.position = [position[0], position[1]]
+        global enemy_id
+        self.id = enemy_id
+        enemy_id += 1
+    def move(self, target:list):
+            ### Ranged enemy movement
+        distance_x = target[0] - self.position[0]
+        distance_y = target[1] - self.position[1]
+        distance = math.sqrt(distance_x**2 + distance_y **2)
+        if distance < self.range: 
+            ranged_espeed_vector = -self.speed
+        elif abs(distance - self.range) < self.speed: ranged_espeed_vector = 0 
+        else: ranged_espeed_vector = self.speed
+        if self.position[0] < target[0]: self.position[0] += ranged_espeed_vector
+        elif self.position[0] > target[0]: self.position[0] -= ranged_espeed_vector
+        if self.position[1] < target[1]: self.position[1] += ranged_espeed_vector
+        elif self.position[1] > target[1]: self.position[1] -= ranged_espeed_vector
+
+
+def addenemy(which:int):
+    global enemy_list
+    if which == 0: enemy_list.append(enemy(speed = 2.5, range = 20, position = (random.randint(0, swidth - pwidth), random.randint(0, sheight- pheight)), type = which))
+    if which == 1: enemy_list.append(enemy(speed = 1, range = 200, position = (random.randint(0, swidth - pwidth), random.randint(0, sheight- pheight)), type = which))
+
+    
 
 ### Clock
 fps = 60
@@ -46,7 +72,7 @@ sheight = 600
 bgcolor = color_black
 screen = pygame.display.set_mode((swidth, sheight))
 pygame.display.set_caption('game.py')
-
+color_list = [color_red, color_white]
 
 ### Main loop
 run = True
@@ -63,6 +89,9 @@ while run:
             if event.key == pygame.K_RIGHT: mov_r = True
             if event.key == pygame.K_UP: mov_u = True
             if event.key == pygame.K_DOWN: mov_d = True
+            if event.key == pygame.K_SPACE:
+                addenemy(random.randint(0,1))
+                print(len(enemy_list))
 
 
         ### Stop moving here
@@ -83,32 +112,11 @@ while run:
     if mov_u and playerpos[1] > 0: playerpos[1] -= moving_speed
     if mov_d and playerpos[1] < sheight - pheight: playerpos[1] += moving_speed
 
-    ### Melee enemy movement
-    if melee_epos[0] + melee_range < playerpos[0]: melee_epos[0] += melee_espeed
-    elif melee_epos[0] - melee_range > playerpos[0]: melee_epos[0] -= melee_espeed
-    if melee_epos[1] + melee_range < playerpos[1]: melee_epos[1] += melee_espeed
-    elif melee_epos[1] -melee_range > playerpos[1]: melee_epos[1] -= melee_espeed
-
-    ### Ranged enemy movement
-    distance_x = playerpos[0] - ranged_epos[0]
-    distance_y = playerpos[1] - ranged_epos[1]
-    distance = math.sqrt(distance_x**2 + distance_y **2)
-    if distance < kiting_distance: 
-        ranged_espeed_vector = -ranged_espeed
-    elif abs(distance - kiting_distance) < ranged_espeed: ranged_espeed_vector = 0 
-    else: ranged_espeed_vector = ranged_espeed
-    if ranged_epos[0] < playerpos[0]: ranged_epos[0] += ranged_espeed_vector
-    elif ranged_epos[0] > playerpos[0]: ranged_epos[0] -= ranged_espeed_vector
-    if ranged_epos[1] < playerpos[1]: ranged_epos[1] += ranged_espeed_vector
-    elif ranged_epos[1] > playerpos[1]: ranged_epos[1] -= ranged_espeed_vector
 
     pygame.draw.rect(screen, color_green, pygame.Rect(playerpos[0], playerpos[1], pwidth, pheight), width = 0)
-    pygame.draw.rect(screen, color_red, pygame.Rect(melee_epos[0], melee_epos[1], pwidth, pheight), width = 0)
-    pygame.draw.rect(screen, color_white, pygame.Rect(ranged_epos[0], ranged_epos[1], pwidth, pheight), width = 0)
-
-
-
-    
+    for bad_guy in enemy_list: 
+        bad_guy.move(playerpos)
+        pygame.draw.rect(screen, color_list[bad_guy.type], pygame.Rect(bad_guy.position[0], bad_guy.position[1], pwidth, pheight), width = 0)
 
     #pygame.display.update()
     pygame.display.flip()
