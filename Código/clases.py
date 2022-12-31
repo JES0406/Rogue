54ed245c2e6a9f588c02b7217acd9f793dd51d61
JES0406 <112687530+JES0406@users.noreply.github.com>
2022-12-31 20:42:13 +0100

    bulleto_4

--- Código/clases.py
+++ Código/clases.py
@@ -178,6 +178,7 @@ class Enemy(pygame.sprite.Sprite):
 
     def update(self, player):
         self.relative_pos = [fn.relative_pos(self.position,0), fn.relative_pos(self.position,1)]
+        self.rect = self.image.get_rect(center = (self.relative_pos[0], self.relative_pos[1]))
         self.move(player)
         self.rect = self.image.get_rect(center = (self.position[0], self.position[1]))
         if self.HP <= 0:
