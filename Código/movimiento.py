0274cb61a93a61371848450606b13af944cceaa0
JES0406 <112687530+JES0406@users.noreply.github.com>
2022-12-31 10:05:24 +0100

    Revert "Merge branch 'main' of https://github.com/JES0406/Rogue"
    This reverts commit b91d696f8ca1d547eb6d4acac06778fd9a0d1e74, reversing
    changes made to 1d88ac2cfb4f8f1f16b7a2bcbba9a935ea104eac.

--- Código/movimiento.py
+++ Código/movimiento.py
@@ -9,7 +9,6 @@ pwidth = 32
 pheight = 32
 playerpos = [ANCHO / 2, ALTO / 2]
 pspeed = 5
-pdamage = 7
 mov_r = False
 mov_l = False
 mov_u = False
@@ -24,8 +23,8 @@ def get_camera_centre(): return (camera_left_top[0] + ANCHO/2, camera_left_top[1
 
 def update_camera_pos():
     global camera_left_top
-    camera_left_top[0] = min(max(0, playerpos[0] - (ANCHO / 2)), mapwidth - ANCHO)
-    camera_left_top[1] = min(max(0, playerpos[1] - (ALTO / 2)), mapheight - ALTO)
+    camera_left_top[0] = min(max(ANCHO / 2, playerpos[0] - ANCHO / 2), mapwidth - ANCHO/2)
+    camera_left_top[1] = min(max(ALTO / 2, playerpos[1] - ALTO / 2), mapheight - ALTO/2)
 
 def relative_pos(position, coordinate:int) -> int: 
     return position[coordinate] - camera_left_top[coordinate]
@@ -33,9 +32,7 @@ def relative_pos(position, coordinate:int) -> int:
 def draw_minimap():
     pygame.draw.rect(screen, 'White', pygame.Rect(0,0,ANCHO / 4 + 2, ALTO / 4 +2))
     pygame.draw.rect(screen, 'Black', pygame.Rect(0,0,ANCHO / 4, ALTO / 4))
-    pygame.draw.rect(screen, color_green, pygame.Rect(playerpos[0]/8,playerpos[1]/8,pheight/8, pheight/8))
-    for enemy in enemy_list:
-        pygame.draw.rect(screen, color_list[enemy.type], pygame.Rect(enemy.position[0]/8,enemy.position[1]/8,pheight/8, pheight/8))
+    pygame.draw.rect(screen, 'White', pygame.Rect(playerpos[0]/8,playerpos[1]/8,pheight/8, pheight/8))
 
 
 def setspeed(): ### Si el jugador se desplaza a la vez en los ejes x e y, tiene que moverse más lento en ambos ejes (si no su velocidad total es considerablemente mayor)
@@ -57,8 +54,8 @@ def calc_distance(point_A:list, point_B:list) -> float:
 enemy_list = []
 enemy_id = 0
 class Enemy:
-    def __init__(self, speed, range, position:tuple, type, hp = 20):
-        self.type, self.speed, self.range, self.position, self.hp = type, speed, range + pwidth, list(position), hp
+    def __init__(self, speed, range, position:tuple, type):
+        self.type, self.speed, self.range, self.position = type, speed, range + pwidth, list(position)
         enemy_list.append(self)
         
     def pathfind(self, target:list):
@@ -71,34 +68,9 @@ class Enemy:
         elif self.position[0] > self.speed + target[0]: self.position[0] -= speed_vector
         if self.position[1] < self.speed + target[1]: self.position[1] += speed_vector
         elif self.position[1] > self.speed + target[1]: self.position[1] -= speed_vector
-
-    def damage(self, hp_loss):
-        self.hp -= hp_loss
-        if self.hp <= 0:
-            enemy_list.remove(self)
     
-projectile_list = []
 class Projectile:
-    def __init__(self, position:list, target_position, size: int, speed: int, damage: int, piercing: bool):
-        self.position = [position[0], position[1]] # Hay que hacerlo así porque si no, solo se crea un pointer a la pos. del jugador
-        self.size, self.damage, self.piercing = size, damage, piercing
-        self.speed_x = speed * (target_position[0]-position[0])/max(calc_distance(self.position, target_position),0.001)
-        self.speed_y = speed * (target_position[1]-position[1])/max(calc_distance(self.position, target_position),0.001)
-        projectile_list.append(self)
-    def update(self):
-        hit_target = False
-        for enemy in enemy_list:
-            if enemy.position[0] < self.position[0] < enemy.position[0] + pheight:
-                if enemy.position[1] < self.position[1] < enemy.position[1] + pheight:
-                    enemy.damage(self.damage)
-                    hit_target = True
-        self.position[0] += self.speed_x
-        self.position[1] += self.speed_y
-        pygame.draw.circle(screen, 'White', (relative_pos(self.position, 0), relative_pos(self.position, 1)), self.size)
-        if not (0 < self.position[0] < mapwidth) or not (0 < self.position[1] < mapheight):
-            self.piercing, hit_target = False, True
-        if hit_target and not self.piercing:
-            projectile_list.remove(self)
+    pass
 
 
 
@@ -131,9 +103,6 @@ while run:
             elif event.key == pygame.K_RIGHT: mov_r = True
             elif event.key == pygame.K_UP: mov_u = True
             elif event.key == pygame.K_DOWN: mov_d = True
-            elif event.key == pygame.K_LSHIFT: 
-                target = (pygame.mouse.get_pos()[0] + camera_left_top[0], pygame.mouse.get_pos()[1] + camera_left_top[1])
-                Projectile(playerpos, target, 10, 2, pdamage, False)
             elif event.key == pygame.K_SPACE:
                 addenemy(random.randint(0,2))
                 print(len(enemy_list))
@@ -166,8 +135,6 @@ while run:
         bad_guy.pathfind(playerpos)
         pygame.draw.rect(screen, color_list[bad_guy.type],\
              pygame.Rect(relative_pos(bad_guy.position, 0), relative_pos(bad_guy.position, 1), pwidth, pheight), width = 0)
-    for projectile in projectile_list:
-        projectile.update()
 
     #pygame.display.update()
     draw_minimap()
